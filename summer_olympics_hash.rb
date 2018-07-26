f43735c952b121ab58a0ba198a98c1bc8b3327b1
twinitiee <twinitiee@gmail.com>
2018-07-26 14:41:07 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -18,6 +18,7 @@ def iterate_through_hash
    summer_olympics = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2012}
   summer_olympics.each do |places, dates|
     puts "The #{places} summer olympics took place in #{dates}."
+  end
 end
 
 def iterate_through_keys
