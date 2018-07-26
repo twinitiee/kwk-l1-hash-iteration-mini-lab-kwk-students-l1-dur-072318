44f0737ab7d1f0fb030c78c02dc1cc4b1d853915
twinitiee <twinitiee@gmail.com>
2018-07-26 14:32:14 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -15,7 +15,8 @@ def add_a_key_value_pair
  puts add_a_key_value_pair
 
 def iterate_through_hash
-  
+   summer_olympics = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2012}
+  summer_olympics.each do
 end
 
 def iterate_through_keys
