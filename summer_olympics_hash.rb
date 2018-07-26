7e807fdeccadbab24256eb8cba4afce2f89ffad9
twinitiee <twinitiee@gmail.com>
2018-07-26 14:17:15 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -6,8 +6,8 @@ def add_a_key_value_pair
  summer_olympics = {Sydney => 2000, Athens => 2004, Beijing => 2008, London => 2012}
  new_place= Atlanta
  new date= 1996
- summer_olympics[new_place] = 1996
-end
+ summer_olympics[new_place] = new_date
+ end
 
 def iterate_through_hash
   summer_olympics.each do |
